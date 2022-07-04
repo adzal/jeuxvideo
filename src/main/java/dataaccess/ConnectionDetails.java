d86232fe05779b71d36e5c32cc96de4f829e1b09
Adzal <adam.lockwood@orange.fr>
2022-07-04 16:21:25 +0200

    ConnectionDetails changed to use lombok's @AllArgsConstructor

--- src/main/java/dataaccess/ConnectionDetails.java
+++ src/main/java/dataaccess/ConnectionDetails.java
@@ -1,38 +1,16 @@
 package dataaccess;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
 public class ConnectionDetails {
 	private String url;
 	private String userName;
 	private String password;
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public ConnectionDetails(String url, String userName, String password) {
-		this.url = url;
-		this.userName = userName;
-		this.password = password;
-	}
-
 }
