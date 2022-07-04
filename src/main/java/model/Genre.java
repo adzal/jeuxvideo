be9862cb95deab2adbda2e0695f1e801f0f05f22
ssanc <ssanc@10.81.200.223>
2022-07-04 16:25:43 +0200

    petit changement de ma part

--- src/main/java/model/Genre.java
+++ src/main/java/model/Genre.java
@@ -1,37 +1,17 @@
 package model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
 public class Genre {
 	private int genreId;
 	private String titre;
 	private String description;
 
-	public int getGenreId() {
-		return genreId;
-	}
-
-	public void setGenreId(int genreId) {
-		this.genreId = genreId;
-	}
-
-	public String getTitre() {
-		return titre;
-	}
-
-	public void setTitre(String titre) {
-		this.titre = titre;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	@Override
-	public String toString() {
-		return "Genre [genreId=" + genreId + ", titre=" + titre + ", description=" + description + "]";
-	}
-
+	
 }
--- src/main/java/model/Jeux.java
+++ src/main/java/model/Jeux.java
@@ -1,5 +1,12 @@
 package model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Jeux {
 	private int jeuxId;
 	private String titre;
@@ -9,86 +16,90 @@ public class Jeux {
 	private String paysOrigine;
 	private String connexion;
 	private String mode;
-	private int genreId;
-
-	public int getJeuxId() {
-		return jeuxId;
-	}
-
-	public void setJeuxId(int jeuxId) {
-		this.jeuxId = jeuxId;
-	}
-
-	public String getTitre() {
-		return titre;
-	}
-
-	public void setTitre(String titre) {
-		this.titre = titre;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public double getPrix() {
-		return prix;
-	}
-
-	public void setPrix(double prix) {
-		this.prix = prix;
-	}
-
-	public java.sql.Date getDateSortie() {
-		return dateSortie;
-	}
-
-	public void setDateSortie(java.sql.Date dateSortie) {
-		this.dateSortie = dateSortie;
-	}
-
-	public String getPaysOrigine() {
-		return paysOrigine;
-	}
-
-	public void setPaysOrigine(String paysOrigine) {
-		this.paysOrigine = paysOrigine;
-	}
-
-	public String getConnexion() {
-		return connexion;
-	}
-
-	public void setConnexion(String connexion) {
-		this.connexion = connexion;
-	}
-
-	public String getJeuxMode() {
-		return mode;
-	}
-
-	public void setMode(String mode) {
-		this.mode = mode;
-	}
-
-	public int getGenreId() {
-		return genreId;
-	}
-
-	public void setGenreId(int genreId) {
-		this.genreId = genreId;
-	}
-
-	@Override
-	public String toString() {
-		return "Jeux [_JeuxId=" + jeuxId + ", _JeuxTitre=" + titre + ", _JeuxDescription=" + description
-				+ ", _JeuxPrix=" + prix + ", _JeuxDateSortie=" + dateSortie + ", _JeuxPaysOrigine="
-				+ paysOrigine + ", _JeuxConnexion=" + connexion + ", _JeuxMode=" + mode + ", _GenreId="
-				+ genreId + "]";
-	}
+	private int genreId; 
+	
+	 // Code Géneré avec Lombock  PLUS BESOIN DE FAIRE DES GETTERS AND SETTERS AUTOMATIQUEMENT +
+	    // STRING TOSTRING
+	
+	
+//	public int getJeuxId() {
+//		return jeuxId;
+//	}
+//
+//	public void setJeuxId(int jeuxId) {
+//		this.jeuxId = jeuxId;
+//	}
+//
+//	public String getTitre() {
+//		return titre;
+//	}
+//
+//	public void setTitre(String titre) {
+//		this.titre = titre;
+//	}
+//
+//	public String getDescription() {
+//		return description;
+//	}
+//
+//	public void setDescription(String description) {
+//		this.description = description;
+//	}
+//
+//	public double getPrix() {
+//		return prix;
+//	}
+//
+//	public void setPrix(double prix) {
+//		this.prix = prix;
+//	}
+//
+//	public java.sql.Date getDateSortie() {
+//		return dateSortie;
+//	}
+//
+//	public void setDateSortie(java.sql.Date dateSortie) {
+//		this.dateSortie = dateSortie;
+//	}
+//
+//	public String getPaysOrigine() {
+//		return paysOrigine;
+//	}
+//
+//	public void setPaysOrigine(String paysOrigine) {
+//		this.paysOrigine = paysOrigine;
+//	}
+//
+//	public String getConnexion() {
+//		return connexion;
+//	}
+//
+//	public void setConnexion(String connexion) {
+//		this.connexion = connexion;
+//	}
+//
+//	public String getJeuxMode() {
+//		return mode;
+//	}
+//
+//	public void setMode(String mode) {
+//		this.mode = mode;
+//	}
+//
+//	public int getGenreId() {
+//		return genreId;
+//	}
+//
+//	public void setGenreId(int genreId) {
+//		this.genreId = genreId;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Jeux [_JeuxId=" + jeuxId + ", _JeuxTitre=" + titre + ", _JeuxDescription=" + description
+//				+ ", _JeuxPrix=" + prix + ", _JeuxDateSortie=" + dateSortie + ", _JeuxPaysOrigine="
+//				+ paysOrigine + ", _JeuxConnexion=" + connexion + ", _JeuxMode=" + mode + ", _GenreId="
+//				+ genreId + "]";
+//	}
 
 }
